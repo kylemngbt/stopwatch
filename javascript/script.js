44ea6a9b99b726b58d8a9ddf25aca4d1bc920217
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-08 16:29:03 +0800

    feat: create stopWatch func

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,14 @@
 let [seconds, minutes, hours] = [0,0,0];
 const displayTime = document.querySelector("#display-time");
+
+function stopwatch() {
+  seconds++;
+  if(seconds == 60) {
+    seconds = 0;
+    minutes++;
+    if(minutes == 60) {
+      minutes = 0;
+      hours++;
+    }
+  }
+}
